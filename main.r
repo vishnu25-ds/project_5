b1b5d43d13e1450db2087b05a24f7e5750720b33
vishnu25-ds <vishnu.mathematica@yahoo.com>
2025-07-24 00:08:49 -0500

    Decision tree

--- main.r
+++ main.r
@@ -48,3 +48,51 @@ head(df_final)
 df_final$cluster <- as.factor(df_final$cluster)
 
 ```
+
+
+```{r}
+#####################Decision tree###############################
+set.seed(42)
+library(modelr)
+split <- resample_partition(df_final, c(train=0.75, test = 0.25))
+train_df <- data.frame(split$train)
+test_df <- data.frame(split$test)
+
+
+
+
+
+library(rpart)
+library(rpart.plot)
+
+
+#Run the model
+fit <- rpart(cluster~., data = train_df, method = 'class')
+rpart.plot(fit, extra = 106)
+#Predict on test data:
+predict_unseen <-predict(fit, test_df, type = 'class')
+```
+
+
+```{r}
+table_mat <- table(test_df$cluster, predict_unseen)
+table_mat
+```
+
+```{r}
+
+#Confusion matrix
+predict_unseen
+
+```
+
+
+
+
+```{r}
+accuracy_Test <- sum(diag(table_mat)) / sum(table_mat)
+accuracy_Test
+print(paste('Accuracy for test', round(accuracy_Test,3)))
+
+```
+
